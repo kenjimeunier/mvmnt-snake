2625074d94fbee4d7d24c01423ff6dc1f2654881
kenjimeunier <kenjitoo@gmail.com>
2025-06-09 20:41:01 +0200

    Update frame.py

--- api/frame.py
+++ api/frame.py
@@ -1,12 +1,10 @@
-from .index import app, get_frame
+from .index import get_frame
 import re
 
 def handler(request):
-    # Extraire l'index de l'URL
     path = request.url
     match = re.search(r'/api/frame/(\d+)', path)
     if match:
         index = int(match.group(1))
-        with app.test_request_context():
-            return get_frame(index)
+        return get_frame(index)
     return {'error': 'Invalid frame index'}, 400
